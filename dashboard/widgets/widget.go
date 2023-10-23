32c85eb4b7fb9515d90dfebf654c02b639580746
abruneau <antonin.bruneau@gmail.com>
2023-10-23 14:54:03 +0200

    FIX: fix grid conversion

--- dashboard/widgets/widget.go
+++ dashboard/widgets/widget.go
@@ -3,6 +3,7 @@ package widgets
 import (
 	"fmt"
 	"grafana_to_datadog/grafana"
+	"math"
 
 	"github.com/DataDog/datadog-api-client-go/v2/api/datadogV1"
 )
@@ -39,16 +40,12 @@ func ConvertWidget(source string, panel grafana.Panel) (datadogV1.Widget, error)
 }
 
 func convertGrid(panel grafana.Panel) datadogV1.WidgetLayout {
-	return *datadogV1.NewWidgetLayout(convertMesures(panel.GridPos.H), convertMesures(panel.GridPos.W), convertMesures(panel.GridPos.X), convertMesures(panel.GridPos.Y))
-}
-
-func convertMesures(value int) int64 {
-	if value == 0 {
-		return int64(value)
+	heigh := int64(math.Floor(float64(panel.GridPos.H) / 2))
+	if heigh == 0 {
+		heigh = 1
 	}
-	if value%2 == 0 {
-		return int64(value / 2)
-	} else {
-		return int64((value + 1) / 2)
-	}
+	width := int64(math.Floor(float64(panel.GridPos.W) / 2))
+	x := int64(math.Floor(float64(panel.GridPos.X) / 2))
+	y := int64(math.Floor(float64(panel.GridPos.Y) / 2))
+	return *datadogV1.NewWidgetLayout(heigh, width, x, y)
 }
